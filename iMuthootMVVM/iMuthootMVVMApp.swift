b0cec6be20bf3f8f98708cee40a47cc2f21fb6f6
Rijo <rizosamuel1995@gmail.com>
2021-04-15 12:35:42 +0530

    Initial Commit

---
+++ iMuthootMVVM/iMuthootMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMuthootMVVMApp.swift
+//  iMuthootMVVM
+//
+//  Created by Rijo Samuel on 15/04/21.
+//
+
+import SwiftUI
+
+@main
+struct iMuthootMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
